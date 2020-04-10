7794999c82018f029e809944a5c0923c2b9000b2
Greg <gregory.mkv@gmail.com>
2020-04-10 17:28:59 -0400

    Remove need for sockets_dir

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,46 @@
 use anyhow::Result;
-use neovim_lib::{Neovim, NeovimApi, Session, Value};
-use std::path::PathBuf;
+use neovim_lib::{Neovim, NeovimApi, Session};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 #[structopt(about = "Control nvim from the CLI!")]
 struct Control {
-    #[structopt(flatten)]
-    subcmds: Subcommand,
-    #[structopt(default_value = "/tmp/nvim_rpc")]
-    sockets_dir: PathBuf,
-}
-
-#[derive(StructOpt)]
-enum Subcommand {
-    /// set option=value
-    Set { name: String, value: String },
     /// run an arbitrary command
-    Run { cmd: String },
-}
-
-impl Control {
-    fn run(&self, nvim: &mut Neovim) -> Result<()> {
-        match &self.subcmds {
-            Subcommand::Set { name, value } => NeovimApi::set_option(
-                nvim,
-                name,
-                Value::String(value.as_str().into()),
-            )?,
-            Subcommand::Run { cmd } => nvim.command(cmd)?,
-        };
-        Ok(())
-    }
+    cmd: String,
 }
 
 fn main() -> Result<()> {
     let args = Control::from_args();
+    let tmp = std::env::var("TMPDIR").unwrap_or("/tmp".to_owned());
 
-    match std::fs::read_dir(&args.sockets_dir) {
+    match std::fs::read_dir(tmp) {
         Ok(dir) => dir
             .filter_map(|f| f.ok())
-            .filter_map(|d| Session::new_unix_socket(d.path()).ok())
+            .filter(|f| {
+                let is_dir =
+                    matches!(f.file_type().map(|t| t.is_dir()), Ok(true));
+                let name_heuristic =
+                    f.file_name().to_string_lossy().starts_with("nvim");
+                is_dir && name_heuristic
+            })
+            .filter_map(|dir| {
+                Some(
+                    std::fs::read_dir(dir.path())
+                        .ok()?
+                        .filter_map(Result::ok)
+                        .map(|d| d.path()),
+                )
+            })
+            .flatten()
+            .filter_map(|d| Session::new_unix_socket(d).ok())
             .map(|mut session| {
                 session.start_event_loop();
                 Neovim::new(session)
             })
             .for_each(|mut nvim| {
-                let _ =
-                    args.run(&mut nvim).map_err(|e| eprintln!("Error: {}", e));
+                let _ = nvim
+                    .command(&args.cmd)
+                    .map_err(|e| eprintln!("Error: {}", e));
             }),
         Err(e) if e.kind() == std::io::ErrorKind::NotFound => return Ok(()),
         Err(e) => Err(e)?,
