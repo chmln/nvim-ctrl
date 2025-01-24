7c6b34f2f46f9e3aa98cb8dc3c56c7f040886441
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-01-23 18:42:11 -0800

    Try both TMPDIR (if it exists) and /tmp instead of just one
    I ran into this problem because `nix shell` unsets TMPDIR, so all my
    nvim instances launched from within Nix shells wouldn't respond to
    nvim-ctrl. This fixes that problem

--- src/main.rs
+++ src/main.rs
@@ -15,68 +15,69 @@ struct Control {
 
 fn main() -> Result<()> {
     let args = Control::from_args();
-    let tmp = std::env::var("TMPDIR").unwrap_or("/tmp".to_owned());
 
     let full_file = match args.file {
         None => None,
         Some(ref f) => std::fs::canonicalize(f).ok()
     };
 
-    match std::fs::read_dir(tmp) {
-        Ok(dir) => dir
-            .filter_map(|f| f.ok())
-            .filter(|f| {
-                let is_dir =
-                    matches!(f.file_type().map(|t| t.is_dir()), Ok(true));
-                let name_heuristic =
-                    f.file_name().to_string_lossy().starts_with(&args.vim_type);
-                is_dir && name_heuristic
-            })
-            .filter_map(|dir| {
-                Some(
-                    std::fs::read_dir(dir.path())
-                        .ok()?
-                        .filter_map(Result::ok)
-                        .map(|d| d.path()),
-                )
-            })
-            .flatten()
-            .filter_map(|dir| {
-                Some(
-                    std::fs::read_dir(dir)
-                        .ok()?
-                        .filter_map(Result::ok)
-                        .map(|d| d.path()),
-                )
-            })
-            .flatten()
-            .filter_map(|d| Session::new_unix_socket(d).ok())
-            .map(|mut session| {
-                session.start_event_loop();
-                Neovim::new(session)
-            })
-            .for_each(|mut nvim| {
-                if let Some(full_file) = full_file.clone() {
-                    let full_buf_name = nvim.get_current_buf()
-                        .iter().flat_map(|buf| buf.get_name(&mut nvim))
-                        .flat_map(|bufname| std::fs::canonicalize(bufname))
-                        .collect::<Vec<_>>();
-                    if Some(&full_file) == full_buf_name.get(0) {
+    vec![std::env::var("TMPDIR").ok(), Some(String::from("/tmp"))]
+        .into_iter()
+        .flatten()
+        .filter_map(|tmpdir| std::fs::read_dir(tmpdir).ok())
+        .for_each(|dir| {
+            dir
+                .filter_map(|f| f.ok())
+                .filter(|f| {
+                    let is_dir =
+                        matches!(f.file_type().map(|t| t.is_dir()), Ok(true));
+                    let name_heuristic =
+                        f.file_name().to_string_lossy().starts_with(&args.vim_type);
+                    is_dir && name_heuristic
+                })
+                .filter_map(|dir| {
+                    Some(
+                        std::fs::read_dir(dir.path())
+                            .ok()?
+                            .filter_map(Result::ok)
+                            .map(|d| d.path()),
+                    )
+                })
+                .flatten()
+                .filter_map(|dir| {
+                    Some(
+                        std::fs::read_dir(dir)
+                            .ok()?
+                            .filter_map(Result::ok)
+                            .map(|d| d.path()),
+                    )
+                })
+                .flatten()
+                .filter_map(|d| Session::new_unix_socket(d).ok())
+                .map(|mut session| {
+                    session.start_event_loop();
+                    Neovim::new(session)
+                })
+                .for_each(|mut nvim| {
+                    if let Some(full_file) = full_file.clone() {
+                        let full_buf_name = nvim.get_current_buf()
+                            .iter().flat_map(|buf| buf.get_name(&mut nvim))
+                            .flat_map(|bufname| std::fs::canonicalize(bufname))
+                            .collect::<Vec<_>>();
+                        if Some(&full_file) == full_buf_name.get(0) {
+                            let _ = nvim
+                                .command(&args.cmd)
+                                .map_err(|e| eprintln!("Error: {}", e));
+                        } else {
+                            println!("Skipping non-matching file {:?}", full_buf_name.get(0))
+                        }
+                    } else {
                         let _ = nvim
                             .command(&args.cmd)
                             .map_err(|e| eprintln!("Error: {}", e));
-                    } else {
-                        println!("Skipping non-matching file {:?}", full_buf_name.get(0))
                     }
-                } else {
-                    let _ = nvim
-                        .command(&args.cmd)
-                        .map_err(|e| eprintln!("Error: {}", e));
-                }
-            }),
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => return Ok(()),
-        Err(e) => Err(e)?,
-    }
+                })
+        });
 
     Ok(())
 }
